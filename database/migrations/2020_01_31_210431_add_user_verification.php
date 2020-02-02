ddd301dedf2f58dd646e454bdba56f0f41566df0
bogdan demchenko <bogdandemchenko@gmail.com>
2020-02-02 22:39:20 +0200

    fix migration

--- database/migrations/2020_01_31_210431_add_user_verification.php
+++ database/migrations/2020_01_31_210431_add_user_verification.php
@@ -14,9 +14,13 @@ class AddUserVerification extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('status', 16)->nullable();
+            $table->string('status', 16);
             $table->string('verify_token')->nullable()->unique();
         });
+
+        DB::table('users')->update([
+            'status' => App\Entity\User::STATUS_ACTIVE,
+        ]);
     }
 
     /**
