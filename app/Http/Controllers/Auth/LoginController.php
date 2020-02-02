e350c31004d55867eedaf4f70f9b011826de5bbe
bogdan demchenko <bogdandemchenko@gmail.com>
2020-02-02 15:46:34 +0200

    controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,38 +3,55 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
+use App\Entity\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
+
+//    public function showLoginForm()
+//    {
+//        return view('auth.login');
+//    }
+//
+//    public function login(LoginRequest $request)
+//    {
+//        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+//            $this->hasTooManyLoginAttempts($request)) {
+//            $this->fireLockoutEvent($request);
+//// If the login attempt was unsuccessful we will increment the number of attempts
+//        // to login and redirect the user back to the login form. Of course, when this
+//        // user surpasses their maximum number of attempts they will get locked out.
+//            return $this->sendLockoutResponse($request);
+//        }
+//
+//        if ($this->attemptLogin($request)) {
+//            return $this->sendLoginResponse($request);
+//        }
+//
+//        $this->incrementLoginAttempts($request);
+//
+//        return $this->sendFailedLoginResponse($request);
+//    }
+//
+//    protected function authenticated(Request $request, $user)
+//    {
+//        if (!$user->status !== User::STATUS_ACTIVE) {
+//            $this->guard()->logout();
+//            return back()->with('error', 'You need to confirm your account. Please check your email.');
+//        }
+//
+//        return redirect()->intended($this->redirectPath());
+//    }
 }
