ef020c37eaad0f604ee212ad9c7482255e0cc14a
bogdan demchenko <bogdandemchenko@gmail.com>
2020-02-02 21:39:52 +0200

    console command - verify User

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -6,6 +6,13 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
+        App\Entity\User::create([
+            'name' => 'admin',
+            'email' => 'admin@crm.com',
+            'password' => bcrypt(11111111),
+            'status' => App\Entity\User::STATUS_ACTIVE
+        ]);
+
         factory(App\Entity\User::class, 50)->create();
     }
 }
