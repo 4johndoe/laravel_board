f68ad44e073aa409237a73fb4dee1aa3b2c33682
bogdan demchenko <bogdandemchenko@gmail.com>
2020-02-02 15:44:55 +0200

    move User to custom folder

---
+++ app/Entity/User.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+
+/**
+ * Class User
+ * @package App\Entity
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $status
+ * @property string $password
+ */
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    const STATUS_WAIT = 'wait';
+    const STATUS_ACTIVE = 'active';
+
+    protected $fillable = [
+        'name', 'email', 'password', 'status'
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public static function register(string $name, string $email, string $password): self
+    {
+        return static::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password),
+            'verify_token' => Str::uuid(),
+            'status' => self::STATUS_WAIT
+        ]);
+    }
+
+    public static function new(string $name, string $email): self
+    {
+        return static::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt(Str::random()),
+            'status' => self::STATUS_ACTIVE
+        ]);
+    }
+
+    public function isWait(): bool
+    {
+        return $this->status === self::STATUS_WAIT;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status === self::STATUS_ACTIVE;
+    }
+
+    public function verify(): void
+    {
+        if (!$this->isWait()) {
+            throw new \DomainException('User is already verified.');
+        }
+
+        $this->update([
+            'status' => self::STATUS_ACTIVE,
+            'verify_token' => null,
+        ]);
+    }
+}
